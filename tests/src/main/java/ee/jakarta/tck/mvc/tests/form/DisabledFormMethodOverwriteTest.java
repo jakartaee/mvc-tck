2e8d35cd882d1a4e0029f354244b5e27e0d0df6e
Ivar Grimstad <ivar.grimstad@eclipse-foundation.org>
2025-04-15 11:30:55 +0200

    Update copyright statement

--- tests/src/main/java/ee/jakarta/tck/mvc/tests/form/DisabledFormMethodOverwriteTest.java
+++ tests/src/main/java/ee/jakarta/tck/mvc/tests/form/DisabledFormMethodOverwriteTest.java
@@ -1,5 +1,7 @@
 /*
  * Copyright (c) 2022 Eclipse Krazo committers and contributors
+ * Copyright (c) 2025 Contributors to the Eclipse Foundation
+
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
